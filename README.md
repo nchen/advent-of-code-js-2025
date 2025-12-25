bce8f4b922c8e8a7c47a392b69013ff264cedadf
Neil Chen <chenrong2003@hotmail.com>
2025-12-26 02:05:21 +0800

    init with day 7 input

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code in Javascript
+
+```
+node --version
+v20.17.0
+```
---
+++ day_7.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const input = fs.readFileSync("./inputs/input-7.txt", "utf-8");
+
+console.log(input);
